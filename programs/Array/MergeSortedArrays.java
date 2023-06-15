64096b70cc49dfea96cddc8b1a0a1cd8dcf7dae0
taaaher <taher.ali.hh@gmail.com>
2023-06-16 01:00:02 +0530

    Updated the code

--- programs/Array/MergeSortedArrays.java
+++ programs/Array/MergeSortedArrays.java
@@ -1,2 +1,40 @@
-package dsaWithJava.functions.Array;public class MergeSortedArrays {
+package dsaWithJava.functions.Array;
+
+import java.util.Arrays;
+
+public class MergeSortedArrays {
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3,0,0,0};
+        int[] arr2 = {2,5,6};
+        int m = 3;
+        int n = 3;
+        merge(arr1,m,arr2,n);
+
+    }
+
+    static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int counter = 0;
+        int i = 0;
+        int length = m+n;
+        if (n==0){
+            System.out.println(Arrays.toString(nums1));
+            return;
+        }
+            while (i<length){
+                if (nums1[i] == 0){
+                    nums1[i]=nums2[counter];
+                    counter++;
+                } else if (nums1[i]<nums2[counter]) {
+                    i++;
+                }else {
+                    int temp = nums1[i];
+                    nums1[i] = nums2[counter];
+                    nums2[counter] = temp;
+                }
+                i++;
+            }
+
+        System.out.println(Arrays.toString(nums1));
+    }
+
 }
--- programs/Array/RotateImage.java
+++ programs/Array/RotateImage.java
@@ -1,2 +1,39 @@
-package dsaWithJava.functions.Array;public class RotateImage {
+package dsaWithJava.functions.Array;
+
+import java.util.Arrays;
+
+//https://leetcode.com/problems/rotate-image/description/
+public class RotateImage {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3},{3,4,5},{4,5,2}};
+    }
+
+    static void rotate(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int counter = 0;
+        // Transpose the matrix
+        for (int i = 0; i < row; i++) {
+            for (int j = counter++; j < col; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        int start = 0;
+        int end = col-1;
+        //interchanging the columns till mid.
+        while(start<=end){
+            for (int i = 0; i < row; i++) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+            }
+            start++;
+            end--;
+        }
+        System.out.println(Arrays.deepToString(matrix));
+    }
 }
+
+
