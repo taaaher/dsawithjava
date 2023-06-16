0d56fb867c57ff8938472ae729b7c8eb377ed9f2
taaaher <taher.ali.hh@gmail.com>
2023-06-16 22:21:03 +0530

    Implemented using recursion

--- programs/Recursion/BinarySearch.java
+++ programs/Recursion/BinarySearch.java
@@ -1,2 +1,31 @@
-package dsaWithJava.functions.Recursion;public class BinarySearch {
+package dsaWithJava.functions.Recursion;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {2,3,5,14,56,78,456,3456};
+        System.out.println(search(nums,78,0, nums.length-1));
+    }
+
+    static int search(int[] arr, int target,int start, int end){
+
+        int mid = start+(end-start)/2;
+        //Base condition
+        if(start>end){
+            //Returning the answer
+            return -1;
+        }
+        if(arr[mid] == target){
+            //Returning the answer
+            return mid;
+        }
+        if (arr[mid]>target){
+            //Sub recursion called hence returned.
+            return search(arr,target,start,mid-1);
+        }else {
+            //sub recursion called hence returned.
+            return search(arr,target,mid+1,end);
+        }
+
+    }
+
 }
--- programs/Recursion/Fibonaci.java
+++ programs/Recursion/Fibonaci.java
@@ -1,2 +1,17 @@
-package dsaWithJava.functions.Recursion;public class Fibonaci {
+package dsaWithJava.functions.Recursion;
+
+public class Fibonaci {
+    public static void main(String[] args) {
+        System.out.println(Fibo(7));
+    }
+
+    static int Fibo(int n){
+
+//        Base condition
+        if (n<2){
+            return n;
+        }
+        return Fibo(n-1)+Fibo(n-2);
+    }
+
 }
