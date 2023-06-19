afab69aa26ff31e2bdce86f8f00a5b497c898abd
taaaher <taher.ali.hh@gmail.com>
2023-06-19 16:26:50 +0530

    Optimal Solution which takes O(logn) time

--- programs/Array/Pow_x_n.java
+++ programs/Array/Pow_x_n.java
@@ -2,24 +2,49 @@ package dsaWithJava.functions.Array;
 
 public class Pow_x_n {
     public static void main(String[] args) {
-        double sol = myPow(2.0000,-2147483648);
+        double sol = myPow(2.0000,10);
         System.out.println(sol);
     }
 
 
-    //On the online judge this solution is leading to Time Limit Exceeded error
+
     static double myPow(double x, int n) {
-        double ans = 1;
-        if (n>0){
-            for (int i = 0; i < n; i++) {
-                ans = ans*x;
-            }
-        }else {
-            for (int i = n; i < 0; i++) {
-                ans = ans/x;
+        //Striver's Approach.
+        double ans = 1.0;
+        long nn = n;
+        if(nn<0){
+            nn=nn*-1;
+        }
+        while (nn>0){
+            //Double the value of x if it's even and half the value of n
+            if (nn%2==0){
+                nn=nn/2;
+                x=x*x;
+            }else {
+                //If nn is odd subtract 1 from power and multiply x with the answer.
+                ans=ans*x;
+                nn=nn-1;
             }
         }
+        if (n<0){
+            ans = (double) 1.0/(double) ans;
+        }
         return ans;
+
+
+        //Intitial Solution
+        //On the online judge this solution is leading to Time Limit Exceeded error
+//        double ans = 1;
+//        if (n>0){
+//            for (int i = 0; i < n; i++) {
+//                ans = ans*x;
+//            }
+//        }else {
+//            for (int i = n; i < 0; i++) {
+//                ans = ans/x;
+//            }
+//        }
+//        return ans;
     }
 
 }
