5387d8dd16e8d13628e3b9568a6c51a444d331ba
taaaher <taher.ali.hh@gmail.com>
2023-06-21 22:46:30 +0530

    Added Optimal Solution which uses hashmap and takes NlogN time

--- programs/Array/TwoSum.java
+++ programs/Array/TwoSum.java
@@ -1,6 +1,8 @@
 package dsaWithJava.functions.Array;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 //https://leetcode.com/problems/two-sum/
 
 public class TwoSum {
@@ -11,7 +13,19 @@ public class TwoSum {
         System.out.println(Arrays.toString(ans));
     }
 
+    static int[] twoSum2(int[] nums, int target){
+        //Optimal Solution uses Hashmap and takes O(NlogN) time
+        Map<Integer,Integer> numsToIndex = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numsToIndex.containsKey(target-nums[i])){
+                return new int[] {numsToIndex.get(target-nums[i]),i};
+            }
+            numsToIndex.put(nums[i],i);
+        }
+        return new int[] {};
+    }
     static int[] twoSum(int[] nums, int target) {
+        //Brute force O(N2)
         int[] ans = {-1,-1};
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length; j++) {
