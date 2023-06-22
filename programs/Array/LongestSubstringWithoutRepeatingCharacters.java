230b7a5a010bb96b7642c30173d8f9e47dfd6d38
taaaher <taher.ali.hh@gmail.com>
2023-06-23 00:17:40 +0530

    submitted solution modified

--- programs/Array/LongestSubstringWithoutRepeatingCharacters.java
+++ programs/Array/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,2 +1,31 @@
-package dsaWithJava.functions.Array;public class LongestSubstringWithoutRepeatingCharacters {
+package dsaWithJava.functions.Array;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        int ans = lengthOfLongestSubstring(s);
+        System.out.println(ans);
+    }
+
+    static int lengthOfLongestSubstring(String s) {
+        int len = 0;
+        int right = 0;
+        int left = 0;
+        int n = s.length();
+        HashMap<Character,Integer> mpp = new HashMap<>();
+        while(right<n){
+            if (mpp.containsKey(s.charAt(right))){
+                left = Math.max(mpp.get(s.charAt(right))+1,left);
+            }
+            mpp.put(s.charAt(right),right);
+            len = Math.max(len, right-left+1);
+            right++;
+        }
+
+        return len;
+    }
+
 }
