1fca43b64aedc0f67b5cc44d423f11f6b3b72fb2
taaaher <taher.ali.hh@gmail.com>
2023-08-01 12:36:46 +0530

    binary search problems

--- programs/BinarySearch/FindingMinimumInRotatedSortedArray.java
+++ programs/BinarySearch/FindingMinimumInRotatedSortedArray.java
@@ -1,2 +1,26 @@
-package dsaWithJava.functions.binarySearch;public class FindingMinimumInRotatedSortedArray {
+package dsaWithJava.functions.BinarySearch;
+
+public class FindingMinimumInRotatedSortedArray {
+    public static void main(String[] args) {
+        int [] arr = {5,1,2,3,4};
+        int sol = findMin(arr);
+        System.out.println(sol);
+    }
+
+    static int findMin(int[] nums) {
+        int l = 0 ;
+        int r = nums.length-1;
+
+        while (l < r){
+            int m = (l+r)/2;
+             if(nums[m] > nums[r]){
+                 l = m+1;
+             }else {
+                 r = m;
+             }
+            }
+        return nums[r];
+    }
 }
+
+
--- programs/BinarySearch/KokoEatingBananas.java
+++ programs/BinarySearch/KokoEatingBananas.java
@@ -1,2 +1,38 @@
-package dsaWithJava.functions.binarySearch;public class KokoEatingBananas {
+package dsaWithJava.functions.BinarySearch;
+
+//https://leetcode.com/problems/koko-eating-bananas/
+public class KokoEatingBananas {
+    public static void main(String[] args) {
+        int [] arr = {312884470};
+        int sol = minEatingSpeed(arr, 312884469);
+        System.out.println(sol);
+    }
+
+    static int minEatingSpeed(int[] piles, int h) {
+        int l = 1;
+        int r = 1;
+        int ans = 0;
+//        Finding the maximum from the array
+        for(int pile : piles){
+            r = Math.max(r, pile);
+        }
+
+//        Our search will be between 1 to max element in the array.
+        while(l<r){
+            int hourCount = 0;
+            int m = (l + r) / 2;
+
+            for (int pile: piles){
+                hourCount += (int) Math.ceil((double)pile/ m );
+            }
+//            reason for this condition ?
+             if(hourCount <= h){
+                 r = m;
+             } else {
+                 l = m+1;
+             }
+        }
+        return r;
+    }
+
 }
